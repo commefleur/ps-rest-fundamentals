2c1bfe303ea3a6095722408a39ccd500b30f809e
commefleur <shujun.ye.work@gmail.com>
2025-08-28 22:19:36 -0400

    Fini 3-Improving APIs with Middleware

--- server/src/features/customers/customers.router.ts
+++ server/src/features/customers/customers.router.ts
@@ -1,3 +1,36 @@
 import express from "express";
+import { getCustomerDetail, getCustomers, searchCustomers } from "./customers.service";
+import { getOrdersForCustomer } from "../orders/orders.service";
 
 export const customersRouter = express.Router();
+
+// get list customers
+customersRouter.get("/", async (req, res) => {
+    const customers = await getCustomers();
+    res.json(customers);
+});
+
+// get customer details
+customersRouter.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const customer = await getCustomerDetail(id);
+    if (customer != null) {
+        res.json(customer);
+    } else{
+        res.status(404).json({message: "Customer Not Found"});
+    }
+});
+
+// order subcollection
+customersRouter.get("/:id/orders", async (req, res) => {
+    const id = req.params.id;
+    const orders = getOrdersForCustomer(id);
+    res.json(orders);
+});
+
+// search
+customersRouter.get("/search/:query", async (req, res) => {
+    const query = req.params.query;
+    const customers = await searchCustomers(query);
+    res.json(customers);
+});
--- server/src/features/orders/orders.router.ts
+++ server/src/features/orders/orders.router.ts
@@ -1,3 +1,23 @@
 import express from "express";
+import { getOrderDetail, getOrders } from "./orders.service";
+import { idUUIDRequestSchema, pagingRequestSchema } from "../types";
+import { validate } from "../../middleware/validation.middleware";
 
 export const ordersRouter = express.Router();
+
+// need the take and skip paramaters
+ordersRouter.get("/", validate(pagingRequestSchema), async (req, res) => {
+    const data = pagingRequestSchema.parse(req);
+    const orders = await getOrders(data.query.skip, data.query.take);
+    res.json(orders);
+});
+
+ordersRouter.get("/:id", validate(idUUIDRequestSchema), async (req, res) => {
+    const data = idUUIDRequestSchema.parse(req);
+    const order = await getOrderDetail(data.params.id);
+    if (order != null) {
+        res.json(order);
+    } else {
+        res.status(404).json({message: "Order Not Found"});
+    }
+});
